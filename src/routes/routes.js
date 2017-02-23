6e50350e83167224effc3e846a34e4a30f8047b1
Tom Scott <tomscott85@gmail.com>
2017-02-23 21:46:05 +1100

    build api route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,10 @@
 // import controllers
 import Test from '../controllers/test-controller'
+import UsersController from '../controllers/users-controller'
 
 module.exports = (app) => {
   app.get('/', Test.greeting)
+  app.post('/api/users', UsersController.create)
   // app.get('/api', DriversController.greeting);
   // app.post('/api/drivers', DriversController.create);
   // app.put('/api/drivers/:id', DriversController.edit);  // :id is a wildcard
