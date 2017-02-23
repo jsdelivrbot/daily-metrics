e546009f270695afe87a5acb5aaabd6237b7c77f
Tom Scott <tomscott85@gmail.com>
2017-02-23 21:46:55 +1100

    delete test route/api

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,7 @@
 // import controllers
-import Test from '../controllers/test-controller'
 import UsersController from '../controllers/users-controller'
 
 module.exports = (app) => {
-  app.get('/', Test.greeting)
   app.post('/api/users', UsersController.create)
   // app.get('/api', DriversController.greeting);
   // app.post('/api/drivers', DriversController.create);
